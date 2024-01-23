3200f601a867aa9bda4680a9196692f9328a09b5
Williedaniels <w.daniels@alustudent>
2024-01-23 23:01:50 +0200

    alu-AirBnB_clone_v2

--- models/state.py
+++ models/state.py
@@ -20,18 +20,21 @@ class State(BaseModel, Base):
                               cascade='all, delete-orphan')
 
     else:
-        # if os.getenv("HBNB_TYPE_STORAGE") != "db":
-        # @property
-        # def cities(self):
-        #     """ Returns the list of City instances with state_id
-        #     equals to the current State.id. """
-        #     all_cities = list(models.storage.all(City).values())
-        #     return list(filter(lambda city: (city.id == self.id),
-        #     all_cities))
-        # no time to fix
         @property
         def cities(self):
             """ Returns the list of City instances with state_id
             equals to the current State.id. """
             return [city for city in models.storage.all(City).values()
                     if city.state_id == self.id]
+
+    @classmethod
+    def create_state(cls, California):
+        """ Creates a State instance with the given name """
+        if 'name' in California:
+            new_state = cls(California))
+            models.storage.new(new_state)
+            models.storage.save()
+            return new_state
+        else:
+            print("Error: 'name' is a required argument.")
+            return None
