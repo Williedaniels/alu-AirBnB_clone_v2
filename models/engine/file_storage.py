e73b73e998b175b1e5e9ec2802f6598ac41bb31a
Skaveza <s.mwachoni@alustudent.com>
2024-01-25 18:02:38 +0200

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,28 +23,22 @@ class FileStorage:
 
     def all(self, cls=None):
         
+       def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
         dic = {}
-    if cls:
-        dictionary = self.__objects
-        if cls == State:
-            objs = [obj for obj in dictionary.values() if isinstance(obj, State)]
-            for obj in objs[:2]:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                dic[key] = obj
-        elif cls == City:
-            objs = [obj for obj in dictionary.values() if isinstance(obj, City)]
-            for obj in objs[:1]:
-                key = "{}.{}".format(type(obj).__name__, obj.id)
-                dic[key] = obj
-    else:
-        dictionary = self.__objects
-        objs = [obj for obj in dictionary.values() if isinstance(obj, (State, City))]
-        for obj in objs[:3]:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            dic[key] = obj
-
-    return dic
-
+        if cls:
+            dictionary = self.__objects
+            for key in dictionary:
+                partition = key.replace('.', ' ')
+                partition = shlex.split(partition)
+                if (partition[0] == cls.__name__):
+                    dic[key] = self.__objects[key]
+            return (dic)
+        else:
+            return self.__objects
 
     def new(self, obj):
         """sets __object to given obj
