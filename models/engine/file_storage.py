c7e1925b642542b31c99a6edea34d425e04a64e6
Skaveza <s.mwachoni@alustudent.com>
2024-01-29 22:31:53 +0200

    updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,12 +2,6 @@
 """the file storage class"""
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 import shlex
 
 
@@ -26,15 +20,8 @@ class FileStorage:
         Return:
             returns a dictionary of __object
         """
-        dic = {}
         if cls:
-            dictionary = self.__objects
-            for key in dictionary:
-                partition = key.replace('.', ' ')
-                partition = shlex.split(partition)
-                if (partition[0] == cls.__name__):
-                    dic[key] = self.__objects[key]
-            return (dic)
+            return {key: obj for key, obj in self.__objects.items() if type(obj).__name__ == cls.__name__}
         else:
             return self.__objects
 
@@ -50,9 +37,7 @@ class FileStorage:
     def save(self):
         """serialize the file path to JSON file path
         """
-        my_dict = {}
-        for key, value in self.__objects.items():
-            my_dict[key] = value.to_dict()
+        my_dict = {key: value.to_dict() for key, value in self.__objects.items()}
         with open(self.__file_path, 'w', encoding="UTF-8") as f:
             json.dump(my_dict, f)
 
@@ -61,7 +46,7 @@ class FileStorage:
         """
         try:
             with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                for key, value in (json.load(f)).items():
+                for key, value in json.load(f).items():
                     value = eval(value["__class__"])(**value)
                     self.__objects[key] = value
         except FileNotFoundError:
@@ -72,7 +57,8 @@ class FileStorage:
         """
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
+            if key in self.__objects:
+                del self.__objects[key]
 
     def close(self):
         """ calls reload()
