ba768ab46fcff83cb77d70d55eda556797777d43
Williedaniels <w.daniels@alustudent>
2024-02-16 12:36:33 +0200

    all tasks

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 # sets up the web servers for the deployment of web_static
 
+# Install Nginx if not already installed
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo apt-install -y nginx
+
+# Create the folders
+sudo mkdir -p /data/web_static/{releases,shared}
+sudo mkdir -p /data/web_static/releases/test
+
+# Create a fake HTML file
 echo "webstatic_deployment" | sudo tee /data/web_static/releases/test/index.html
+
+# Create a symbolic link
+sudo rm -f /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start 
+
+# Give ownership of the /data/ folder to the ubuntu user AND group
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update the Nginx configuration
+sudo sed -i '38i\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
